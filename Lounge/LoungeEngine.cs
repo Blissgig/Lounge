112b90db4be23b975bb764a8daa7ec775ec3ae8f
James Rose <blissgig@gmail.com>
2018-02-27 11:41:14 -0500

    Adding audio visualizations

--- Lounge/LoungeEngine.cs
+++ Lounge/LoungeEngine.cs
@@ -7,6 +7,9 @@ using System.Reflection;
 using System.Windows.Forms;
 using Lounge.Models;
 using System.Collections.ObjectModel;
+using Un4seen.Bass;
+using Un4seen.BassWasapi;
+using System.Windows.Threading;
 
 namespace Lounge
 {
@@ -24,6 +27,8 @@ namespace Lounge
         ObservableCollection<FileFolderData> filesFolders = new ObservableCollection<FileFolderData>();
 
         private List<DirectoryInfo> breadcrumbs = new List<DirectoryInfo>();
+
+        private Analyzer loungeAnalyzer;
         #endregion
 
         public LoungeEngine(MainWindow window)
@@ -34,6 +39,8 @@ namespace Lounge
 
                 SettingsLoad(); //Call before anything else runs.
                 
+                loungeAnalyzer = new Analyzer(window);
+
                 ListFiles(null);
             }
             catch (Exception ex)
@@ -47,6 +54,10 @@ namespace Lounge
             try
             {
                 applicationName = AppName();  //This is because of the need for this value in some functions that are threaded
+
+                string sBassNetEmail = ""; //TODO
+                string sBassNetRegistration = "";
+                BassNet.Registration(sBassNetEmail, sBassNetRegistration); //for audio visualization //TODO
 
                 if (IsWin10() == true)
                 {
@@ -64,9 +75,9 @@ namespace Lounge
 
 
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                throw;
+                logException(ex);
             }
         }
 
@@ -329,6 +340,194 @@ namespace Lounge
             catch { }
         }
 
+    }
+
+    public class AudioDeviceInfo
+    {
+        public int DeviceID { get; set; }
+        public string Name { get; set; }
+    }
+
+    internal class Analyzer
+    {
+        private bool _enable;               //enabled status
+        private DispatcherTimer _t;         //timer that refreshes the display
+        private byte mbTimerTime = 25;
+        public float[] _fft;               //buffer for fft data
+        private WASAPIPROC _process;        //callback function to obtain data
+        private int _lastlevel;             //last output level
+        private int _hanctr;                //last output level counter
+        public List<byte> BediaSpectrumData = new List<byte>();   //spectrum data buffer
+        private List<AudioDeviceInfo> AudioDevices = new List<AudioDeviceInfo>();     //NEW non-UI device list
+        private bool _initialized;          //initialized flag
+        private int devindex;               //used device index
+        private MainWindow mainWindow = null;
+        private int _lines = 64;            // number of spectrum lines
+
+
+        public Analyzer(MainWindow Window)
+        {  
+            try
+            {
+                mainWindow = Window;
+
+                _fft = new float[8192];
+                _lastlevel = 0;
+                _hanctr = 0;
+                _t = new DispatcherTimer();
+                _t.Tick += _t_Tick;
+                _t.Interval = TimeSpan.FromMilliseconds(mbTimerTime); 
+                _process = new WASAPIPROC(Process);
+                _initialized = false;
+
+                Init();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        
+        // flag for display enable
+        public bool DisplayEnable { get; set; }
+
+        //flag for enabling and disabling program functionality
+        public bool Enable
+        {
+            get { return _enable; }
+            set
+            {
+                _enable = value;
+                if (value)
+                {
+                    if (!_initialized)
+                    {
+                        devindex = AudioDevices[mainWindow.audioDevices.SelectedIndex].DeviceID;
+
+                        bool result = BassWasapi.BASS_WASAPI_Init(devindex, 0, 0, BASSWASAPIInit.BASS_WASAPI_BUFFER, 1f, 0.05f, _process, IntPtr.Zero);
+                        if (!result)
+                        {
+                            var error = Bass.BASS_ErrorGetCode();
+                        }
+                        else
+                        {
+                            _initialized = true;
+                        }
+                    }
+                    BassWasapi.BASS_WASAPI_Start();
+                }
+                else BassWasapi.BASS_WASAPI_Stop(true);
+                System.Threading.Thread.Sleep(500);
+                _t.IsEnabled = value;
+            }
+        }
+
+        // initialization
+        private void Init()
+        {
+            bool result = false;
+            int iCount = BassWasapi.BASS_WASAPI_GetDeviceCount();
+
+            for (int i = 0; i < iCount; i++)
+            {
+                var device = BassWasapi.BASS_WASAPI_GetDeviceInfo(i);
+                if (device.IsEnabled && device.IsLoopback)
+                {
+                    AudioDeviceInfo audioDeviceInfo = new AudioDeviceInfo();
+                    audioDeviceInfo.DeviceID = i; //device.id  //TODO SHOULDN'T THIS BE THE RIGHT VALUE... AND YET IT WORKS
+                    audioDeviceInfo.Name = device.name;
+                    AudioDevices.Add(audioDeviceInfo);
+
+                    mainWindow.audioDevices.Items.Add(device.name);
+
+                    //TO SET THE SPEAKER OUTPUT
+                    //TODO: save setting and get it here
+                    if (device.name.ToLower().IndexOf("speaker") > -1)
+                    {
+                        mainWindow.audioDevices.SelectedIndex = (mainWindow.audioDevices.Items.Count - 1);
+                    }
+                }
+            }
+            
+            Bass.BASS_SetConfig(BASSConfig.BASS_CONFIG_UPDATETHREADS, false);
+            result = Bass.BASS_Init(0, 44100, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero);
+            if (!result) throw new Exception("Init Error");
+        }
+
+        //timer 
+        private void _t_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                int ret = BassWasapi.BASS_WASAPI_GetData(_fft, (int)BASSData.BASS_DATA_FFT8192);  //get ch.annel fft data
+                if (ret < -1) return;
+                int x, y;
+                int b0 = 0;
+
+                //computes the spectrum data, the code is taken from a bass_wasapi sample.
+                for (x = 0; x < _lines; x++)
+                {
+                    float peak = 0;
+                    int b1 = (int)Math.Pow(2, x * 10.0 / (_lines - 1));
+                    if (b1 > 1023) b1 = 1023;
+                    if (b1 <= b0) b1 = b0 + 1;
+                    for (; b0 < b1; b0++)
+                    {
+                        if (peak < _fft[1 + b0]) peak = _fft[1 + b0];
+                    }
+
+                    y = (int)(Math.Sqrt(peak) * 3 * 255 - 4);
+                    if (y > 255)
+                    {
+                        y = 255;
+                    }
+                    else if (y < 0)
+                    {
+                        y = 0;
+                    }
+                    BediaSpectrumData.Add((byte)y);
+                }
+
+                //TODO: SET VISUALIZATION HERE
+               // mainWindow.SetVisualisation(BediaSpectrumData);
+
+
+                BediaSpectrumData.Clear();
+
+                int level = BassWasapi.BASS_WASAPI_GetLevel();
+                if (level == _lastlevel && level != 0) _hanctr++;
+                _lastlevel = level;
+
+                //Required, because some programs hang the output. If the output hangs for a 75ms
+                //this piece of code re initializes the output so it doesn't make a gliched sound for long.
+                if (_hanctr > 3)
+                {
+                    _hanctr = 0;
+                    Free();
+                    Bass.BASS_Init(0, 44100, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero);
+                    _initialized = false;
+                    Enable = true;
+                }
+
+            }
+            catch (Exception)
+            {
+                //eat the error, I do not care atm
+            }
+        }
+
+        // WASAPI callback, required for continuous recording
+        private int Process(IntPtr buffer, int length, IntPtr user)
+        {
+            return length;
+        }
+
+        //cleanup
+        public void Free()
+        {
+            BassWasapi.BASS_WASAPI_Free();
+            Bass.BASS_Free();
+        }
     }
 
 }
