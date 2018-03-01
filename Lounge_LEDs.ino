22733a582167fc47dc0e104e644dac5b93fb350a
James Rose <blissgig@gmail.com>
2018-03-01 18:05:51 -0500

    A simple script for LEDs
    This is a small piece of code, COMPLETELY (atm) taken from the FastLED framework for the Ardino.    This is just a starting point.   Once more of the C# code is done that app will affect this script.

---
+++ Lounge_LEDs.ino
@@ -0,0 +1,60 @@
+#include "FastLED.h"
+
+FASTLED_USING_NAMESPACE
+
+// Simple LED sketch.  Completely pirated from the Demo from FastLED
+
+#if defined(FASTLED_VERSION) && (FASTLED_VERSION < 3001000)
+#warning "Requires FastLED 3.1 or later; check github for latest code."
+#endif
+
+#define DATA_PIN    5
+#define LED_TYPE    WS2811
+#define COLOR_ORDER GRB
+#define NUM_LEDS    300
+#define BRIGHTNESS  88
+#define FRAMES_PER_SECOND  120
+#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
+
+uint8_t gHue = 0; // rotating "base color" 
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  delay(3000); // 3 second delay for recovery
+  
+  // tell FastLED about the LED strip configuration
+  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+
+  // set master brightness control
+  FastLED.setBrightness(BRIGHTNESS);  
+}
+
+void loop()
+{
+  confetti();
+
+  // send the 'leds' array out to the actual LED strip
+  FastLED.show();  
+  // insert a delay to keep the framerate modest
+  FastLED.delay(1000/FRAMES_PER_SECOND); 
+
+  // do some periodic updates
+  EVERY_N_MILLISECONDS( 200 ) { gHue++; } 
+}
+
+void confetti() 
+{
+  // random colored speckles that blink in and fade smoothly
+  fadeToBlackBy( leds, NUM_LEDS, 10);
+  int pos = random16(NUM_LEDS);
+  leds[pos] += CHSV( gHue + random8(64), 200, 255);
+  addGlitter(80);
+}
+
+void addGlitter( fract8 chanceOfGlitter) 
+{
+  if( random8() < chanceOfGlitter) {
+    leds[ random16(NUM_LEDS) ] += CRGB::White;
+  }
+}
